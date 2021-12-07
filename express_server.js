b900790d3f8de3b43e4fb75fa185c33549d2dc42
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2021-12-07 04:09:08 +0000

    Redirecting after submission and short urls

--- express_server.js
+++ express_server.js
@@ -49,9 +49,18 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString(); 
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  
 });
 
 
