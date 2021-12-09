db647db05b99481780ef1129692820e806c56db5
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2021-12-09 04:55:48 +0000

    debugging

--- express_server.js
+++ express_server.js
@@ -17,8 +17,22 @@ const generateRandomString = function() {
 
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {
+      longURL: "http://www.lighthouselabs.ca",
+      userID: "user1ID"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "user2ID"
+  },
+  "1sm2xK": {
+    longURL: "http://www.facebook.com",
+    userID: "user1ID"
+  },
+  "123vxK": {
+    longURL: "http://www.instagram.com",
+    userID: "user1ID"
+  }
 };
 
 const users = { 
@@ -30,7 +44,7 @@ const users = {
  "user2ID": {
     id: "user2ID", 
     email: "2@a.com", 
-    password: "1234"
+    password: "123"
   }
 }
 
@@ -44,13 +58,27 @@ const findUser = (newEmail) => {
   return false;
 };
 
+const urlsForUser = (id) => {
+  const obj = {};
+  for (const key in urlDatabase) {
+    if (urlDatabase[key]["userID"] === id) {
+      obj[key] = urlDatabase[key];
+    }
+  }
+  return obj;
+};
+
 
 
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
 const bodyParser = require("body-parser");
+const { request } = require("express");
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -65,13 +93,22 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
+
+  if (!req.cookies.user_id) {
+    return res.status(404).send("You need to login/register to view the TinyURLs");
+  }
+
+  const obj = urlsForUser(req.cookies.user_id)
   //const templateVars = {urls: urlDatabase, username: req.cookies["username"]};     // assigns existing object (urlDatabase) as a value to key urls in a new object
-  const templateVars = {urls: urlDatabase, user: users[req.cookies.user_id]};
+  const templateVars = {urls: obj, user: users[req.cookies.user_id]};
   res.render("urls_index", templateVars);   // renders "urls_index" ejs & templateVars can be accessed in that file (See for.. in loop)
 });
 
 app.get("/urls/new", (req, res) => {
 
+  if (!req.cookies.user_id) {
+    res.redirect("/login");
+  }
   //const templateVars = {urls: urlDatabase, username: req.cookies["username"]};
   const templateVars = {urls: urlDatabase, user: users[req.cookies.user_id]};
   res.render("urls_new", templateVars );
@@ -79,7 +116,18 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {    /// ????? what does it do?
   //const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};   // params is parameters
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies.user_id]};
+  if (!urlDatabase[req.params.shortURL]) {
+    return res.status(403).send("id does not exist");
+  }
+
+  const urlId = req.params.shortURL;
+  const cookieId = req.cookies.user_id;
+  
+  if (urlDatabase[urlId].userID !== String(req.cookies.user_id)) {
+    return res.status(403).send("URL doesn't belong to user");
+  }
+
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[req.cookies.user_id]};
   res.render("urls_show", templateVars);  // renders "urls_show" ejs and templateVars can be accessed in the ejs file
 });
 
@@ -87,7 +135,7 @@ app.get("/u/:shortURL", (req, res) => {   // anything after : is a wild card & c
   
   //const templateVars = {urls: urlDatabase, username: req.cookies["username"]}; <= doesn't go here because it redirects outside of tinyapp
 
-  const longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);    // for redirecting to the actual webpage using the shortURL
 });
 
@@ -108,25 +156,56 @@ app.get("/login", (req, res) => {
 
 
 app.post("/urls", (req, res) => {
+  
+  if (!req.cookies.user_id) {
+    return res.status(404).send("You need to login to create/modify a TinyURL");
+  }
+
   const shortURL = generateRandomString();  // generating a random string using above function for shortURL
   const longURL = req.body.longURL;       // body is what's being submitted via form/submit button
-  urlDatabase[shortURL] = longURL;        // assigning a new key-value pair in object (database)
+  urlDatabase[shortURL] = {longURL: longURL, userID: req.cookies.user_id};        // assigning a new key-value pair in object (database)
   res.redirect(`/urls/${shortURL}`);
-  console.log(req.body);  // Log the POST request body to the console  
+   
   
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {  // for deleting
+  
+  if (!req.cookies.user_id) {
+    return res.status(403).send("You need to login to create/modify a TinyURL");
+  }
+
+  const url = req.params.shortURL;
+  const cookieId = req.cookies.user_id;
+  
+  if (urlDatabase[url].userID !== String(req.cookies.user_id)) {
+    return res.status(403).send("URL doesn't belong to user");
+  }
+
+
+
   const urlId = req.params.shortURL;    //
   delete urlDatabase[urlId];
   res.redirect("/urls");
 });
 
 app.post("/urls/:id", (req, res) => {  // for editing/updating
+  
+  if (!req.cookies.user_id) {
+    return res.status(403).send("You need to login to create/modify a TinyURL");
+  }
+
+  const url = req.params.id;
+  const cookieId = req.cookies.user_id;
+  
+  if (urlDatabase[url].userID !== String(req.cookies.user_id)) {
+    return res.status(403).send("URL doesn't belong to user");
+  }
+  
   const urlId = req.params.id;           // extracting id
   const newLongUrl = req.body.newURL;    // extracting new/edited url submitted from the body (using .newURL because that's what it's named in url_show)
 
-  urlDatabase[urlId] = newLongUrl;      // assigning it as a value directly to the key in object/urlDatabase
+  urlDatabase[urlId].longURL = newLongUrl;      // assigning it as a value directly to the key in object/urlDatabase
 
   res.redirect("/urls");
 });
