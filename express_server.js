2fbcdd2947bbaef5c0fb4a925a10c94a8c5b9e25
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2021-12-10 04:23:20 +0000

    final few changes

--- express_server.js
+++ express_server.js
@@ -6,16 +6,6 @@ const app = express();
 const bcrypt = require("bcryptjs");
 const salt = bcrypt.genSaltSync(10);
 
-
-
-const PORT = 8080; // default port 8080
-
-app.set("view engine", "ejs");
-
-
-// const cookieParser = require("cookie-parser");
-// app.use(cookieParser());
-
 const cookieSession = require("cookie-session");
 app.use(cookieSession ({
   name: 'session',
@@ -26,19 +16,13 @@ const bodyParser = require("body-parser");
 const { request } = require("express");
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.set("view engine", "ejs");
+
+const PORT = 8080; // default port 8080
 
 
-
-
-const generateRandomString = function() {
-  let randomString = "";
-  for (let i = 0; i < 6; i++) {
-    const randomCharCode = Math.floor(Math.random() * 26 + 97);
-    const randomChar = String.fromCharCode(randomCharCode);
-    randomString += randomChar;
-  }
-  return randomString;
-};
+// const cookieParser = require("cookie-parser");
+// app.use(cookieParser());
 
 
 const urlDatabase = {
@@ -73,15 +57,17 @@ const users = {
   }
 }
 
-// const findUser = (newEmail, users) => {
-//   for (const id in users) {
-//     const user = users[id];
-//     if (user["email"] === newEmail) {
-//       return user;
-//     }
-//   }
-//   return false;
-// };
+
+const generateRandomString = function() {
+  let randomString = "";
+  for (let i = 0; i < 6; i++) {
+    const randomCharCode = Math.floor(Math.random() * 26 + 97);
+    const randomChar = String.fromCharCode(randomCharCode);
+    randomString += randomChar;
+  }
+  return randomString;
+};
+
 
 const urlsForUser = (id) => {
   const obj = {};
@@ -94,178 +80,148 @@ const urlsForUser = (id) => {
 };
 
 
-
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+// Home
+app.get("/", (req, res) => {
+  if (!req.session.user_id) {
+    return res.redirect("/login");
+  } else {
+    return res.redirect("/urls");
+  }
 });
+
+
+// Example
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
 
 app.get("/urls", (req, res) => {
 
-  // if (!req.cookies.user_id) {
-  //   return res.status(403).send("You need to login/register to view the TinyURLs");
-  // }
-
-  if (!req.session.user_id) {
-    return res.status(403).send("You need to login/register to view the TinyURLs");
+  const obj = urlsForUser(req.session.user_id); 
+  const templateVars = {urls: obj, user: users[req.session.user_id]};     // assigns existing object (urlDatabase) as a value to key urls in a new object
+  
+  if (!req.session.user_id) {      // Checks for a cookie session to know whether user logged in or not
+    return res.status(403).render("urls_index", templateVars);
   }
 
-  // const obj = urlsForUser(req.cookies.user_id);
-  const obj = urlsForUser(req.session.user_id);
-  //const templateVars = {urls: urlDatabase, username: req.cookies["username"]};     // assigns existing object (urlDatabase) as a value to key urls in a new object
-  // const templateVars = {urls: obj, user: users[req.cookies.user_id]};
-  const templateVars = {urls: obj, user: users[req.session.user_id]};
-  res.render("urls_index", templateVars);   // renders "urls_index" ejs & templateVars can be accessed in that file (See for.. in loop)
+  res.render("urls_index", templateVars);      // renders "urls_index" ejs & templateVars can be accessed in that file (See for.. in loop)
 });
 
-app.get("/urls/new", (req, res) => {
 
-  // if (!req.cookies.user_id) {
-  //   res.redirect("/login");
-  // }
-
+app.get("/urls/new", (req, res) => {   // Takes it to "create url"
+ 
   if (!req.session.user_id) {
-    res.redirect("/login");
+    return res.redirect("/login");
   }
-
-  //const templateVars = {urls: urlDatabase, username: req.cookies["username"]};
-  // const templateVars = {urls: urlDatabase, user: users[req.cookies.user_id]};
 
   const templateVars = {urls: urlDatabase, user: users[req.session.user_id]};
   res.render("urls_new", templateVars );
 });
 
-app.get("/urls/:shortURL", (req, res) => {    /// ????? what does it do?
-  //const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};   // params is parameters
+
+app.get("/urls/:shortURL", (req, res) => {    // Takes it to "edit url"
+  
+  if (!req.session.user_id) {
+    return res.status(403).send("You need to login/register to view the TinyURLs");
+  }
+  
   if (!urlDatabase[req.params.shortURL]) {
     return res.status(404).send("id does not exist");
   }
 
   const urlId = req.params.shortURL;
-  // const cookieId = req.cookies.user_id;
   const cookieId = req.session.user_id;
-  
-  // if (urlDatabase[urlId].userID !== String(req.cookies.user_id)) {
-  //   return res.status(401).send("URL doesn't belong to user");
-  // }
 
   if (urlDatabase[urlId].userID !== String(req.session.user_id)) {
     return res.status(401).send("URL doesn't belong to user");
   }
 
-  // const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[req.cookies.user_id]};
-
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[req.session.user_id]};
   res.render("urls_show", templateVars);  // renders "urls_show" ejs and templateVars can be accessed in the ejs file
 });
 
+
 app.get("/u/:shortURL", (req, res) => {   // anything after : is a wild card & can be accessed by req.params
   
-  //const templateVars = {urls: urlDatabase, username: req.cookies["username"]}; <= doesn't go here because it redirects outside of tinyapp
+  if (!urlDatabase[req.params.shortURL]) {
+    return res.status(404).send("URL does not exist");
+  }
 
   const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);    // for redirecting to the actual webpage using the shortURL
 });
 
-app.get("/register", (req, res) => {
-  //const templateVars = {urls: urlDatabase, username: req.cookies["username"]}; 
-  // const templateVars = {urls: urlDatabase, user: users[req.cookies.user_id]};
 
+app.get("/register", (req, res) => {
+  
+  if (req.session.user_id) {
+    return res.redirect("/urls");
+  }
   const templateVars = {urls: urlDatabase, user: users[req.session.user_id]};
   res.render("register", templateVars);
 });
 
-app.get("/login", (req, res) => { 
-  // const templateVars = {urls: urlDatabase, user: users[req.cookies.user_id]};
 
+app.get("/login", (req, res) => { 
+
+  if (req.session.user_id) {
+    return res.redirect("/urls");
+  }
   const templateVars = {urls: urlDatabase, user: users[req.session.user_id]};
   res.render("login", templateVars);
 });
 
 
-
-
-
-
 app.post("/urls", (req, res) => {
   
-  // if (!req.cookies.user_id) {
-  //   return res.status(403).send("You need to login to create/modify a TinyURL");
-  // }
-
   if (!req.session.user_id) {
     return res.status(403).send("You need to login to create/modify a TinyURL");
   }
 
   const shortURL = generateRandomString();  // generating a random string using above function for shortURL
   const longURL = req.body.longURL;       // body is what's being submitted via form/submit button
-  // urlDatabase[shortURL] = {longURL: longURL, userID: req.cookies.user_id};        // assigning a new key-value pair in object (database)
   
-  urlDatabase[shortURL] = {longURL: longURL, userID: req.session.user_id};
+  urlDatabase[shortURL] = {longURL: longURL, userID: req.session.user_id};  // assigning a new key-value pair in object (database)
   res.redirect(`/urls/${shortURL}`);
-   
-  
 });
 
-app.post("/urls/:shortURL/delete", (req, res) => {  // for deleting
-  
-  // if (!req.cookies.user_id) {
-  //   return res.status(403).send("You need to login to create/modify a TinyURL");
-  // }
+
+app.post("/urls/:shortURL/delete", (req, res) => {  // for deleting   // Minor part left ***
 
   if (!req.session.user_id) {
     return res.status(403).send("You need to login to create/modify a TinyURL");
   }
 
   const url = req.params.shortURL;
-  // const cookieId = req.cookies.user_id;
   const cookieId = req.session.user_id;
-  
-  // if (urlDatabase[url].userID !== String(req.cookies.user_id)) {
-  //   return res.status(401).send("URL doesn't belong to user");
-  // }
 
   if (urlDatabase[url].userID !== String(req.session.user_id)) {
     return res.status(401).send("URL doesn't belong to user");
   }
 
-
-
-  const urlId = req.params.shortURL;    //
+  const urlId = req.params.shortURL;
   delete urlDatabase[urlId];
   res.redirect("/urls");
 });
 
-app.post("/urls/:id", (req, res) => {  // for editing/updating
-  
-  // if (!req.cookies.user_id) {
-  //   return res.status(403).send("You need to login to create/modify a TinyURL");
-  // }
 
+app.post("/urls/:id", (req, res) => {      // for editing/updating
+  
   if (!req.session.user_id) {
     return res.status(403).send("You need to login to create/modify a TinyURL");
   }
 
   const url = req.params.id;
-  // const cookieId = req.cookies.user_id;
   const cookieId = req.session.user_id;
-  
-  // if (urlDatabase[url].userID !== String(req.cookies.user_id)) {
-  //   return res.status(401).send("URL doesn't belong to user");
-  // }
   
   if (urlDatabase[url].userID !== req.session.user_id) {
     return res.status(401).send("URL doesn't belong to user");
   }
-
 
   const urlId = req.params.id;           // extracting id
   const newLongUrl = req.body.newURL;    // extracting new/edited url submitted from the body (using .newURL because that's what it's named in url_show)
@@ -275,43 +231,35 @@ app.post("/urls/:id", (req, res) => {  // for editing/updating
   res.redirect("/urls");
 });
 
+
 app.post("/login", (req, res) => {
   
   const email = req.body.email;
   const password = req.body.password;
   const hashedPassword = bcrypt.hashSync(password, 10);
   
-  //const userName = req.body.username;
-  if (!email || !password) {
+  if (!email || !password) {                  // Checks for blank credentials
     return res.status(400).send("Email/Password cannot be blank!");
   }
 
   const isUser = findUser(email, users);
 
-  if (!isUser) {
+  if (!isUser) {         // Checks for email id
     return res.status(400).send("Email address not found");
   } 
-
-  // if (isUser.password !== password) {
-  //   return res.status(400).send("Password doesn't match");
-  // }
   
   const result = bcrypt.compareSync(password, isUser.password);
 
-  if (result) {
-    //res.cookie("user_id", isUser.id);
+  if (result) {                             // Checks whether hashed passwords are matching or not
     req.session.user_id = isUser.id;
-    // res.cookie("username", userName);
     res.redirect("/urls"); 
   } else {
     return res.status(400).send("Password doesn't match");
   }
-
 });
 
 app.post("/logout", (req, res) => {
   const id = req.body.user_id;
-  // res.clearCookie("user_id", id);
   req.session = null;
   res.redirect("/urls");
 });
@@ -337,9 +285,6 @@ app.post("/register", (req, res) => {
 
   users[id] = {id, email, "password": hashedPassword}; 
 
-  //console.log(users[id]);
-
-  //res.cookie("user_id", id);
   req.session.user_id = id;
   
   res.redirect("urls");
